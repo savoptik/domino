b7b1f979fc61f23d28891a3968b931b5c9ae578d
savoptik <sav70792@gmail.com>
2018-09-11 20:44:50 +0300

    добавлен метод чтения имени игрока и комментарии к коду.

--- Domino/game/player.swift
+++ Domino/game/player.swift
@@ -8,22 +8,38 @@
 
 import Foundation
 
+// игрок наследуется от "базара", он имеет имя, может брать из "базара" костяжки, и выдавать костяжку, если у него есть подходящая.
 public class playr: setOfBones {
-    let name: String
+    private let name: String
     
+    // принимаем в конструкторе имя игорька
     public init(playerName: String) {
+        // записываем его в приватное поле
         self.name = playerName
     }
     
+    // метод вернёт имя игорька
+    public func playerName() -> String {return name}
+    
+    // метод принимает объект "базара" и берёт из него костяшку
     public func askForBone(bones: setOfBones) {
+        // получаем нечто из "Базара
         let b = bones.takeKnuckle()
+        // проверяем, что там пришло, если не nill
         if b != nil {
+            // берём костяшку
             Dominos.append(b!)
         }
     }
     
+    // метод получает варианты того, что требуется положить
     public func takeKnuckle(left: Int, right: Int) -> bone? {
         var indicesOfMatchingTiles: [Int] = []
+        // проверяем, есть ли игроку чего давать?
+        if Dominos.count == 0 {
+            return nil
+        }
+        // собираем индексы подходящих костяшек
         for i in 0...Dominos.count-1 {
             if Dominos[i].left == left {
                 indicesOfMatchingTiles.append(i)
@@ -35,17 +51,20 @@ public class playr: setOfBones {
                 indicesOfMatchingTiles.append(i)
             }
         }
+        // если нет подходящих костяшек, возвращаем nill
         if indicesOfMatchingTiles.isEmpty {
             return nil
         }
         var resultIndex: Int = 0
         var maxSum: Int = 0
+        // выбираем костяжку, от которой больше всего хотим избавиться
         for i in 0...indicesOfMatchingTiles.count-1 {
             if (Dominos[indicesOfMatchingTiles[i]].left + Dominos[indicesOfMatchingTiles[i]].right) > maxSum {
                 resultIndex = indicesOfMatchingTiles[i]
                 maxSum = Dominos[indicesOfMatchingTiles[i]].left + Dominos[indicesOfMatchingTiles[i]].right
             }
         }
+        // извлекаем её из своего набора
         let res: bone = Dominos[resultIndex]
         Dominos.remove(at: resultIndex)
         return res
--- Domino/game/setDominoes.swift
+++ Domino/game/setDominoes.swift
@@ -12,24 +12,27 @@ public class setOfBones {
     var Dominos: [bone] = []
     
     public init() {
-        let doubleZero = bone(leftValue: 0, rightValue: 0)
-        self.Dominos.append(doubleZero)
-        for i in 0...5 {
+        // заполняем "базар" костяшками, должно быть 28
+        for i in 0...6 {
             for j in i...6 {
                 self.Dominos.append(bone(leftValue: i, rightValue: j))
             }
         }
-        let doubleSix = bone(leftValue: 6, rightValue: 6)
-        self.Dominos.append(doubleSix)
     }
     
+    // метод выдаёт случайную костяшку
     public func takeKnuckle() -> bone? {
+        // проверяем, остались ли ещё в "базаре" костяшки?
         if Dominos.count == 0 {
-            return nil
+            return nil // возвращаем nill если базар пуст
         }
-        let number = arc4random_uniform(UInt32(Dominos.count))
+        // получаем индекс случайной костяшки
+        let number = arc4random_uniform(UInt32(Dominos.count - 1))
+        // копируем костяшку
         let returnBone: bone = Dominos[Int(number)]
+        // удаляем вынутую костяшку из базара
         Dominos.remove(at: Int(number))
+        // возвращаем костяшку
         return returnBone
     }
 }
