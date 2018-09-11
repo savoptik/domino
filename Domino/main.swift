e721bc0d4c8a0267eba955b8ff4892ed99db731c
savoptik <sav70792@gmail.com>
2018-09-11 14:41:45 +0300

    Initial Commit

---
+++ Domino/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Domino
+//
+//  Created by Артём Семёнов on 11/09/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
